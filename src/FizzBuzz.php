3a9f63359966239acb58bb06c1715b2f4dbf00e1
Vanessacor <vanessapcbarros@gmail.com>
2020-10-21 18:49:52 +0200

    add tests and complete Kata

---
+++ src/FizzBuzz.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+class FizzBuzz {
+
+  public function evaluate($number) {
+    if($number % 3 === 0 && $number % 5 === 0) {
+      return "FizzBuzz";
+    }
+    if($number % 3 === 0) {
+      return "Fizz";
+    }
+    if($number % 5 === 0) {
+      return "Buzz";
+    }
+
+    return $number;
+  }
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,15 +3,46 @@
 namespace Tests;
 
 use  PHPUnit\Framework\TestCase;
+use App\FizzBuzz;
 
 
-class ExampleTest extends TestCase {
+class FizzBuzzTest extends TestCase {
 
-	public function test_example(
+	public function test_is_number_multiple_3(
 	) {
 
-		$this->assertEquals(4, 4);
+		$fizzBuzz = new FizzBuzz();
+		$result = $fizzBuzz->evaluate(3);
+
+		$this->assertEquals("Fizz", $result);
 	}
+
+	public function test_is_number_multiple_5(
+		) {
+	
+			$fizzBuzz = new FizzBuzz();
+			$result = $fizzBuzz->evaluate(5);
+	
+			$this->assertEquals("Buzz", $result);
+		}
+
+		public function test_is_number_multiple_3_and_5(
+			) {
+		
+				$fizzBuzz = new FizzBuzz();
+				$result = $fizzBuzz->evaluate(15);
+		
+				$this->assertEquals("FizzBuzz", $result);
+			}
+
+		public function test_is_number_not_multiple_3_or_5(
+			) {
+		
+				$fizzBuzz = new FizzBuzz();
+				$result = $fizzBuzz->evaluate(8);
+		
+				$this->assertEquals(8, $result);
+			}
 
 
 }
