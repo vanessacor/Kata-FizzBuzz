7276405342ca6d011761757fe6d32aeef0db17c0
Vanessacor <vanesapcbarros@gmail.com>
2020-10-22 14:08:44 +0200

    fix formatting

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -26,23 +26,23 @@ class FizzBuzzTest extends TestCase {
 			$this->assertEquals("Buzz", $result);
 		}
 
-		public function test_is_number_multiple_3_and_5(
-			) {
-		
-				$fizzBuzz = new FizzBuzz();
-				$result = $fizzBuzz->evaluate(15);
-		
-				$this->assertEquals("FizzBuzz", $result);
-			}
+	public function test_is_number_multiple_3_and_5(
+		) {
+	
+			$fizzBuzz = new FizzBuzz();
+			$result = $fizzBuzz->evaluate(15);
+	
+			$this->assertEquals("FizzBuzz", $result);
+		}
 
-		public function test_is_number_not_multiple_3_or_5(
-			) {
-		
-				$fizzBuzz = new FizzBuzz();
-				$result = $fizzBuzz->evaluate(8);
-		
-				$this->assertEquals(8, $result);
-			}
+	public function test_is_number_not_multiple_3_or_5(
+		) {
+	
+			$fizzBuzz = new FizzBuzz();
+			$result = $fizzBuzz->evaluate(8);
+	
+			$this->assertEquals(8, $result);
+		}
 
 
 }
